57e79c68b43b57b376a3be3da02177559c5a5aa5
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2019-04-14 15:22:55 -0700

    Some update

---
+++ assets/js/scrolly.js
@@ -0,0 +1,7 @@
+$(document).on('click', '.scrolly', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 1000);
+});
